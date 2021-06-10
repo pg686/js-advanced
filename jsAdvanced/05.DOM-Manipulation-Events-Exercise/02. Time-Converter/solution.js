1b5ae7946b05ca77316bc8758bcf710831324b23
pg686 <83918472+pg686@users.noreply.github.com>
2021-06-10 20:30:17 +0300

    upload

--- jsAdvanced/05.DOM-Manipulation-Events-Exercise/02. Time-Converter/solution.js
+++ jsAdvanced/05.DOM-Manipulation-Events-Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,37 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+let daysBtn = document.getElementById('daysBtn');
+let hoursBtn = document.getElementById('hoursBtn');
+let minutesBtn = document.getElementById('minutesBtn');
+let secondsBtn = document.getElementById('secondsBtn');
+
+let days = document.getElementById('days');
+let hours = document.getElementById('hours');
+let minutes = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+
+daysBtn.addEventListener('click' , function(){
+    seconds.value = Number(days.value) * 24*60*60;
+    minutes.value = Number(days.value) * 24*60;
+    hours.value = Number(days.value) * 24;
+});
+
+
+hoursBtn.addEventListener('click' , function(){
+    seconds.value = Number(hours.value) * 60*60;
+    minutes.value = Number(hours.value) *60 ;
+    days.value = Number(hours.value) / 24;
+});
+
+minutesBtn.addEventListener('click' , function(){
+    seconds.value = Number(minutes.value) * 60;
+    hours.value = Number(minutes.value) /60 ;
+    days.value = Number(minutes.value) / 24 / 60;
+});
+
+secondsBtn.addEventListener('click' , function(){
+    hours.value = Number(seconds.value) /60/60;
+    minutes.value = Number(seconds.value) /60 ;
+    days.value = Number(seconds.value) / 24 /60 /60;
+});
 }
--- jsAdvanced/05.DOM-Manipulation-Events-Exercise/03. Locked-Profile/app.js
+++ jsAdvanced/05.DOM-Manipulation-Events-Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,17 @@
 function lockedProfile() {
-    console.log('TODO...')
+    console.log('TODO...');
+    let buttons = [...document.getElementsByTagName('button')];
+    let profiles = document.querySelectorAll('.profile')
+    buttons.forEach((button,index) => {
+        buttons[index].addEventListener('click' , ()=>{
+            let lockedValue = profiles[index].children[2]
+            let hiddenFields = document.getElementById(`user${index+1}HiddenFields`)
+        if(!lockedValue.checked){
+            hiddenFields.style.display=  hiddenFields.style.display === 'block' ? 'none' : 'block'
+        button.textContent = hiddenFields.style.display === 'block' ? 'Hide it' : 'Show more'
+        }
+        
+             }
+        )
+    })
 }
