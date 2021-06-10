77f66eeb67c8acbcec309d22680bc8e7b63f3b83
pg686 <83918472+pg686@users.noreply.github.com>
2021-06-10 15:48:33 +0300

    upload

--- jsAdvanced/05.DOM-Manipulation-Events-Exercise/01. Sections/script.js
+++ jsAdvanced/05.DOM-Manipulation-Events-Exercise/01. Sections/script.js
@@ -1,3 +1,18 @@
 function create(words) {
    console.log('TODO:...');
+   let content = document.getElementById('content');
+   words.forEach(element => {
+      let newDiv = document.createElement('div');
+      let paragraph = document.createElement('p');
+      paragraph.textContent = element;
+      paragraph.style.display = 'none';
+      newDiv.appendChild(paragraph);
+      newDiv.addEventListener('click', function click(e){
+         e.target.children[0].style.display = 'block';
+      })
+      content.appendChild(newDiv);
+
+   });
+
+   
 }
