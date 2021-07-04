ddbfd79b114a47883f4db4c17fad8f3585eb325a
pg686 <83918472+pg686@users.noreply.github.com>
2021-07-04 13:37:21 +0300

    upload solutions

---
+++ js-applications/02.Asynchronous-Programming-Lab/02.Github-Repos/scripts/app.js
@@ -0,0 +1,30 @@
+function loadRepos() {
+	console.log("TODO...");
+	let button = document.querySelector('body button');
+	let listRepos = document.querySelector('#repos');
+	button.addEventListener('click' , function() {
+		let input = document.querySelector('#username');
+		let usernameVal = input.value;
+		let baseUrl = 'https://api.github.com/users/';
+		fetch(`${baseUrl}${usernameVal}/repos`)
+		.then((response) => response.json())
+		.then((data) => data.forEach(element => {
+			let resultLi = createEl(element.full_name,element.html_url);
+			listRepos.appendChild(resultLi)
+		}))
+		.catch((error) =>console.log(error))
+	})
+
+function createEl(fullName, htmlUrl){
+	let newLi = document.createElement('li');
+	let newA = document.createElement('a');
+	newA.href = htmlUrl;
+	newA.textContent = fullName;
+	newLi.appendChild(newA);
+	return newLi;
+}
+}
+
+//  <a href="{repo.html_url}">
+//{repo.full_name}
+//</a>
