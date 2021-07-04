44bff822c4b2bd6e3dcbc51a2a18f4809081c143
pg686 <83918472+pg686@users.noreply.github.com>
2021-07-04 15:07:00 +0300

    uploading solution to 03.Github-Commits

--- js-applications/02.Asynchronous-Programming-Lab/03.Gighub-Commits/app.js
+++ js-applications/02.Asynchronous-Programming-Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,49 @@
 function loadCommits() {
     // Try it with Fetch API
     console.log('TODO...');
+    let button = document.querySelector('body button');
+    let commitsUl = document.querySelector('#commits');
+    button.addEventListener('click', add);
+
+
+    function  add()  {
+        
+        let inputUsername = document.querySelector('#username');
+        let inputRepo = document.querySelector('#repo');
+        let username = inputUsername.value;
+        let repo = inputRepo.value;
+        const baseUrl = 'https://api.github.com/repos';
+        fetch(`${baseUrl}/${username}/${repo}/commits`)
+        .then((res) => 
+         {
+            if(res.ok){
+return res.json();
+            }else{
+                console.log(res.status)
+               // console.log(res.statusText)
+                throw new Error(res.status);
+            }
+        })
+        
+        .then((data) => { 
+            commitsUl.innerHTML= '';
+           // console.log(data)
+data.forEach(element => {
+   let result = createEl(element.commit.author.name,element.commit.message);
+commitsUl.appendChild(result);
+});
+        })
+        .catch((err) => {
+            commitsUl.innerHTML= '';     
+let newLi = createEl(err.message);
+commitsUl.appendChild(newLi);
+        });
+        inputUsername.value = '';
+        inputRepo.value = '';
+    }
+    function createEl(firstArg, commitMessage){
+        let liEl = document.createElement('li');
+        liEl.textContent =   commitMessage === undefined ? `Error: ${firstArg} (Not Found)` : `${firstArg}: ${commitMessage}`;
+        return liEl;
+    }
 }
