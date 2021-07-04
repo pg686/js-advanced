04639c42c66031089c8b8c68744316df1c7170cb
pg686 <83918472+pg686@users.noreply.github.com>
2021-07-04 13:53:23 +0300

    fixing some errors

--- js-applications/02.Asynchronous-Programming-Lab/02.Github-Repos/scripts/app.js
+++ js-applications/02.Asynchronous-Programming-Lab/02.Github-Repos/scripts/app.js
@@ -3,16 +3,24 @@ function loadRepos() {
 	let button = document.querySelector('body button');
 	let listRepos = document.querySelector('#repos');
 	button.addEventListener('click' , function() {
+		listRepos.innerHTML = '';
 		let input = document.querySelector('#username');
 		let usernameVal = input.value;
 		let baseUrl = 'https://api.github.com/users/';
 		fetch(`${baseUrl}${usernameVal}/repos`)
-		.then((response) => response.json())
-		.then((data) => data.forEach(element => {
+		.then((response) => {
+			if(response.ok){
+			return response.json()
+			}
+		})
+		.then((data) => {
+		console.log(data)
+		data.forEach(element => {
 			let resultLi = createEl(element.full_name,element.html_url);
 			listRepos.appendChild(resultLi)
-		}))
-		.catch((error) =>console.log(error))
+		})})
+		.catch((error) =>console.log(error));
+		input.value ='';
 	})
 
 function createEl(fullName, htmlUrl){
