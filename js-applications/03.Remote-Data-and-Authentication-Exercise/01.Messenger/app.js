54146db66e538cf131b06abdd3c30f03a54c54d7
pg686 <83918472+pg686@users.noreply.github.com>
2021-07-17 00:50:31 +0300

    uploading solutions

--- js-applications/03.Remote-Data-and-Authentication-Exercise/01.Messenger/app.js
+++ js-applications/03.Remote-Data-and-Authentication-Exercise/01.Messenger/app.js
@@ -1,5 +1,51 @@
 function attachEvents() {
     console.log('TODO...');
+    let submitBtn = document.querySelector('#submit');
+    let refreshBtn = document.querySelector('#refresh');
+    let author = document.querySelector('#author');
+    let content = document.querySelector('#content');
+    let textarea = document.querySelector('#messages');
+
+    console.log(author.value);
+    console.log(content.value);
+    submitBtn.addEventListener('click', submitData);
+    refreshBtn.addEventListener('click', getAllMessages);
+
+    async function getAllMessages(){
+try{
+        let baseUrl = 'http://localhost:3030/jsonstore/messenger';
+let response = await fetch(baseUrl);
+let data  = await response.json();
+
+textarea.value = Object.values(data).map(element => `${element.author}: ${element.content}`).join('\n');
+}catch(err){
+    console.error(err)
+}
+    }
+
+    async function submitData(){
+        try{
+
+       let body = JSON.stringify({
+           
+  author: author.value,
+  content: content.value,
+
+       })
+       let result = await  fetch('http://localhost:3030/jsonstore/messenger' , {
+           method: 'post',
+           headers: {
+            'Content-Type' : 'application/json'
+        },
+        body
+       });
+       let  create = await result.json();
+        textarea.value += `\n${create.author}: ${create.content}`;
+    }catch(err){
+        console.error(err)
+    }
+}
+
 }
 
 attachEvents();
--- js-applications/03.Remote-Data-and-Authentication-Exercise/04.Book-library/app.js
+++ js-applications/03.Remote-Data-and-Authentication-Exercise/04.Book-library/app.js
@@ -1,1 +1,127 @@
 console.log('My requests...')
+let loadBtn = document.querySelector('#loadBooks');
+let form = document.querySelector('form');
+
+form.addEventListener('submit', submitHandler);
+function editFunc(e){
+let currentBook = e.target.closest('.book');
+let currentTitle = currentBook.querySelector('.title');
+let currentAuthor = currentBook.querySelector('.author');
+ form.dataset.entryId = currentBook.id;
+ form.dataset.isEdit = true;
+let formH3 = form.querySelector('h3');
+formH3.textContent='Edit Form'
+let inputTitle = form.querySelector('input[name="title"]')
+let inputAuthor = form.querySelector('input[name="author"]')
+inputTitle.value = currentTitle.textContent;
+inputAuthor.value = currentAuthor.textContent;
+
+
+
+}
+
+function submitHandler(e)
+{
+    e.preventDefault();
+    let form1 = e.currentTarget;
+    let formData = new FormData(form1 );
+    let id = form1.dataset.entryId;
+    if(form1.dataset.isEdit !== undefined){
+
+        fetch(`http://localhost:3030/jsonstore/collections/books/${id}`,{
+            method:'put',
+            headers: {
+               'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+               title: formData.get('title'),
+               author: formData.get('author'),
+      
+            })
+    })
+    form1.dataset.isEdit = undefined;
+    form1.dataset.entryId = undefined;
+    form1.querySelector('h3').textContent = 'Form'
+
+}else{
+    fetch('http://localhost:3030/jsonstore/collections/books', {
+        method:'post',
+        headers: {
+           'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+           title: data.get('title'),
+           author: data.get('author')
+        })
+    })
+
+    }
+    
+
+}
+loadBtn.addEventListener('click', getAllBooks);
+let allBooks = document.querySelector('tbody');
+
+async function getAllBooks(){
+    try{
+        allBooks.querySelectorAll('tr').forEach(el => el.remove());
+    let response = await fetch('http://localhost:3030/jsonstore/collections/books');
+    let data = await response.json();
+   Object.values(data).map( el => {
+    let newTr  = document.createElement('tr');
+    newTr.setAttribute('id', el._id);
+    newTr.classList.add('book');
+    let td1  = document.createElement('td');
+    td1.classList.add('title');
+    td1.textContent = el.title;
+
+    let td2  = document.createElement('td');
+    td2.textContent = el.author;
+    td2.classList.add('author');
+    let td3  = document.createElement('td');
+    let editBtn = document.createElement('button');
+    editBtn.textContent = 'Edit';
+    editBtn.addEventListener('click' , editFunc);
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.addEventListener('click', deleteBook)
+    td3.appendChild(editBtn);
+    td3.appendChild(deleteBtn);
+
+    newTr.appendChild(td1);
+    newTr.appendChild(td2);
+    newTr.appendChild(td3);
+    console.log(newTr);
+allBooks.appendChild(newTr);
+    
+   })
+
+    }catch(err){
+        console.error(err)
+    }
+
+}
+
+function deleteBook(e){
+    let currentBook = e.target.closest('.book');
+
+let currentId = currentBook.id;
+fetch(`http://localhost:3030/jsonstore/collections/books/${currentId}` , {
+    method: 'delete'
+}).then((res)=> {
+    if(res.ok){
+        currentBook.remove();
+    }
+}).catch((err) => console.error(err))
+
+}
+//<tbody>
+//            <tr>
+//                <td>Harry Poter</td>
+//                <td>J. K. Rowling</td>
+//                <td>
+//                    <button>Edit</button>
+//                    <button>Delete</button>
+//                </td>
+//            </tr>
